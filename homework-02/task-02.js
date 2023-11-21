4cfac4c7f60edb657145ce98d245587a4278b904
enkomium <96664472+enkomium@users.noreply.github.com>
2023-11-21 18:10:58 +0300

    Решение задач
    Решает 2, 3 и 4 задачи

---
+++ homework-02/task-02.js
@@ -0,0 +1,34 @@
+// Напиши скрипт подсчета стоимости гравировки украшений.
+// Для этого создай функцию calculateEngravingPrice(message, pricePerWord)
+// принимающую строку(в строке будут только слова и пробелы) и цену гравировки одного слова, и
+// возвращающую цену гравировки всех слов в строке.
+// ---------------------------------------------------------------------------------------------
+
+// const calculateEngravingPrice = function (message, pricePerWord) {
+//   message = message.split(' ');
+//   const result = message.length * pricePerWord;
+
+//   return result;
+// };
+
+// console.log(
+//   calculateEngravingPrice(
+//     'Proin sociis natoque et magnis parturient montes mus',
+//     10,
+//   ),
+// ); // 80
+
+// console.log(
+//   calculateEngravingPrice(
+//     'Proin sociis natoque et magnis parturient montes mus',
+//     20,
+//   ),
+// ); // 160
+
+// console.log(
+//   calculateEngravingPrice('Donec orci lectus aliquam est magnis', 40),
+// ); // 240
+
+// console.log(
+//   calculateEngravingPrice('Donec orci lectus aliquam est magnis', 20),
+// ); // 120
---
+++ homework-02/task-03.js
@@ -0,0 +1,23 @@
+// Напиши фукцнию findLongestWord(string),
+// которая принимает параметром произвольную строку(в строке будут только слова и пробелы) и
+// возвращает самое длинное слово в этой строке.
+// -------------------------------------------------------------------------------------------
+
+// const findLongestWord = function (string) {
+//   string = string.split(' ');
+//   let small = string[0];
+
+//   for (const word of string) {
+//     if (word.length > small.length) {
+//       small = word;
+//     }
+//   }
+
+//   return small;
+// };
+
+// console.log(findLongestWord('The quick brown fox jumped over the lazy dog')); // 'jumped'
+
+// console.log(findLongestWord('Google do a roll')); // 'Google'
+
+// console.log(findLongestWord('May the force be with you')); // 'force'
