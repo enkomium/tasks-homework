772c4275b930af6e6b066bca58e9f101721974f7
enkomium <96664472+enkomium@users.noreply.github.com>
2023-11-23 18:54:43 +0300

    Решение.
    Решает задачу номер 5.

---
+++ homework-02/task-05.js
@@ -0,0 +1,17 @@
+// Напиши функцию checkForSpam(message), принимающую 1 параметр message - строку.
+// Функция проверяет ее на содержание слов spam и sale.
+// Если нашли зарещенное слово то функция возвращает true, если запрещенных слов нет функция возвращает false.
+// Слова в строке могут быть в произвольном регистре.
+
+const checkForSpam = function (message) {
+  message = message.toLowerCase().replace(/[^a-z\s]/g, '');
+  return `${message} - ${message.includes('spam') || message.includes('sale')}`;
+};
+
+console.log(checkForSpam('Latest technology news')); // false
+
+console.log(checkForSpam('JavaScript weekly newsletter')); // false
+
+console.log(checkForSpam('Get best sale offers now!')); // true
+
+console.log(checkForSpam('[SPAM] How to earn fast money?')); // true
