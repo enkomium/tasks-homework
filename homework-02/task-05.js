6ab59762c7173502b9bdb125e3166eb9fadd3af1
enkomium <96664472+enkomium@users.noreply.github.com>
2023-11-24 17:12:32 +0300

    Решение +1
    Решает задачу №6

--- homework-02/task-05.js
+++ homework-02/task-05.js
@@ -3,15 +3,15 @@
 // Если нашли зарещенное слово то функция возвращает true, если запрещенных слов нет функция возвращает false.
 // Слова в строке могут быть в произвольном регистре.
 
-const checkForSpam = function (message) {
-  message = message.toLowerCase().replace(/[^a-z\s]/g, '');
-  return `${message} - ${message.includes('spam') || message.includes('sale')}`;
-};
+// const checkForSpam = function (message) {
+//   message = message.toLowerCase().replace(/[^a-z\s]/g, '');
+//   return `${message} - ${message.includes('spam') || message.includes('sale')}`;
+// };
 
-console.log(checkForSpam('Latest technology news')); // false
+// console.log(checkForSpam('Latest technology news')); // false
 
-console.log(checkForSpam('JavaScript weekly newsletter')); // false
+// console.log(checkForSpam('JavaScript weekly newsletter')); // false
 
-console.log(checkForSpam('Get best sale offers now!')); // true
+// console.log(checkForSpam('Get best sale offers now!')); // true
 
-console.log(checkForSpam('[SPAM] How to earn fast money?')); // true
+// console.log(checkForSpam('[SPAM] How to earn fast money?')); // true
